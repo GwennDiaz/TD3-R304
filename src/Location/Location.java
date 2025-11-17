ed88e9877c7e62c47f0d62615ec4051b17b204e2
MEGNUCHE riad <riad.megnouche@etu.univ-amu.fr>
2025-11-17 12:08:23 +0100

    Création de la class chef de clan et ses caractéristiques

--- src/Location/Location.java
+++ src/Location/Location.java
@@ -1,4 +1,6 @@
 package Location;
+
+import Characters.Character; // Important : Import de votre classe Character
 import Characters.Gallics.Gallic;
 import java.util.*;
 import static java.lang.Math.max;
@@ -7,7 +9,6 @@ public abstract class Location {
     protected String name;
     protected double area;
     protected Gallic clanChief;
-    protected int numberOfCharactersPresent;
     protected List<Character> presentCharacters;
     protected List<String> presentFood;
 
@@ -24,12 +25,18 @@ public abstract class Location {
     public String getName() { return name; }
     public double getArea() { return area; }
     public Gallic getClanChief() { return clanChief; }
+
     public int getNumberOfCharactersPresent() {
-        return presentCharacters.size();}
+        return presentCharacters.size();
+    }
+
     public List<Character> getPresentCharacters() {
-        return presentCharacters; }
+        return presentCharacters;
+    }
+
     public List<String> getPresentFood() {
-        return presentFood; }
+        return presentFood;
+    }
 
     // --- SETTERS---
 
@@ -39,11 +46,64 @@ public abstract class Location {
     public void setClanChief(Gallic clanChief) {
         this.clanChief = clanChief;
     }
+
     public abstract String getType();
 
-    //Method who add a new character
+    // --- METHODS ---
+
+    // Add a character
     public void addCharacter(Character c) {
         this.presentCharacters.add(c);
         System.out.println(c.getName() + " entered into " + this.name);
+    }
+
+    // Remove a character
+    public void removeCharacter(Character c) {
+        if (presentCharacters.contains(c)) {
+            presentCharacters.remove(c);
+            System.out.println(c.getName() + " left " + this.name);
+        } else {
+            System.out.println(c.getName() + " is not in " + this.name);
+        }
+    }
+
+    // Display characteristics
+    public void printStatus() {
+        System.out.println("\n--- STATUS OF " + name.toUpperCase() + " ---");
+        System.out.println("Type: " + getType());
+        System.out.println("Area: " + area + " m²");
+        if (clanChief != null) {
+            System.out.println("Chief: " + clanChief.getName());
+        }
+        System.out.println("Food available: " + presentFood);
+        System.out.println("Characters present (" + getNumberOfCharactersPresent() + "):");
+        for (Character c : presentCharacters) {
+            System.out.println(" - " + c.toString());
+        }
+        System.out.println("-----------------------------------\n");
+    }
+
+    // Heal characters
+    public void healAllCharacters() {
+        System.out.println("Healing everyone in " + name + "...");
+        for (Character c : presentCharacters) {
+            c.setHealth(100); // Remet la santé à 100
+        }
+        System.out.println("All characters have been healed!");
+    }
+
+    // Feed characters
+    public void feedAllCharacters() {
+        System.out.println("Feeding time in " + name + "...");
+
+        for (Character c : presentCharacters) {
+            if (!presentFood.isEmpty()) {
+                String food = presentFood.removeFirst();
+                c.setHunger(0);
+                System.out.println(c.getName() + " ate " + food + ".");
+            } else {
+                System.out.println("No more food for " + c.getName() + "!");
+            }
+        }
     }
 }
