9fb68cfe599a7a164ca03a18e00e23d82ca8b7a6
GwennDiaz <gwenn.diaz@etu.univ-mu.fr>
2025-12-03 10:02:05 +0100

    j'ai enlevé le abstract

--- src/Characters/Romans/Legionnaire.java
+++ src/Characters/Romans/Legionnaire.java
@@ -6,7 +6,7 @@ import Characters.Gender;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
-public abstract class Legionnaire extends Roman {
+public class Legionnaire extends Roman {
     private String legion;
     private boolean enService;
 
